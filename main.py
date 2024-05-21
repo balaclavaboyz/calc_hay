fb3d9b9de2b295dedab91b930b0754d1cb49bde4
peter <peterthomaskim@gmail.com>
2024-05-20 23:05:42 -0300

    added new var to search

--- main.py
+++ main.py
@@ -13,48 +13,51 @@ if __name__ == '__main__':
 
     xmls_entrada = [i for i in glob('./entrada/*.xml')]
 
-    for i in xmls_entrada:
-        with open(i) as f:
-            domtree = parse(f)
+    if xmls_entrada:
+        for i in xmls_entrada:
+            with open(i) as f:
+                domtree = parse(f)
 
-        name = domtree.getElementsByTagName('nNF')[0].firstChild.data
-        item = domtree.getElementsByTagName('cProd')[0].firstChild.data
-        vProd = domtree.getElementsByTagName('vProd')[0].firstChild.data
-        indTot = domtree.getElementsByTagName('indTot')[0].firstChild.data
+            name = domtree.getElementsByTagName('nNF')[0].firstChild.data
+            item = domtree.getElementsByTagName('cProd')[0].firstChild.data
+            vProd = domtree.getElementsByTagName('vProd')[0].firstChild.data
+            indTot = domtree.getElementsByTagName('indTot')[0].firstChild.data
+            data_hora_emissao = domtree.getElementsByTagName('dhEmi')[0].firstChild.data
 
-        cur.execute('''
-            insert into estoque(
-            id,
-            price,
-            date,
-            qnt,
-            entrada)
-            values(?,?,datetime(),?,?)
-        ''', (name, vProd, int(indTot), 1)
-        )
+            cur.execute('''
+                insert into estoque(
+                id,
+                price,
+                date,
+                qnt,
+                entrada)
+                values(?,?,?,?,?)
+            ''', (name, vProd, data_hora_emissao(), int(indTot), 1)
+            )
 
     xml_saida = [i for i in glob('./saida/*.xml')]
+    if xml_saida:
+        for file in xml_saida:
+            with open(file) as f:
+                domtree = parse(f)
 
-    for file in xml_saida:
-        with open(file) as f:
-            domtree = parse(f)
+            name = domtree.getElementsByTagName('nNF')[0].firstChild.data
+            item = domtree.getElementsByTagName('cProd')[0].firstChild.data
+            vProd = domtree.getElementsByTagName('vProd')[0].firstChild.data
+            indTot = domtree.getElementsByTagName('indTot')[0].firstChild.data
+            data_hora_emissao = domtree.getElementsByTagName('dhEmi')[0].firstChild.data
 
-        name = domtree.getElementsByTagName('nNF')[0].firstChild.data
-        item = domtree.getElementsByTagName('cProd')[0].firstChild.data
-        vProd = domtree.getElementsByTagName('vProd')[0].firstChild.data
-        indTot = domtree.getElementsByTagName('indTot')[0].firstChild.data
-
-        # saida
-        cur.execute('''
-            insert into estoque(
-            id,
-            price,
-            date,
-            qnt,
-            entrada)
-            values(?,?,datetime(),?,?)
-        ''', (name, vProd, int(indTot), 0)
-        )
+            # saida
+            cur.execute('''
+                insert into estoque(
+                id,
+                price,
+                date,
+                qnt,
+                entrada)
+                values(?,?,?,?,?)
+            ''', (name, vProd, data_hora_emissao, int(indTot), 0)
+            )
 
     cur.close()
     con.commit()
