8ee4ab146113177f746eac6db7efdeceb8ae7459
Peter Kim <peterthomaskim@gmail.com>
2024-05-27 16:58:03 -0300

    added new view => view count prod

--- db.py
+++ db.py
@@ -8,7 +8,15 @@ class Db:
     def init(self, con: sqlite3.Connection, cur: sqlite3.Cursor):
         cur.execute('drop table if exists estoque')
         cur.execute(
-            'create table estoque(id text, natop text, price real, name text, date timestamp, qnt int, entrada int, nfe text, foreign key (nfe) references nfe (nfe))')
+            '''create table estoque(id text,
+            natop text,
+            price real,
+            name text,
+            date timestamp,
+            qnt int,
+            entrada int,
+            nfe text,
+            foreign key (nfe) references nfe (nfe))''')
 
         cur.execute('drop table if exists pred')
         cur.execute('create table if not exists pred(id text, pred text)')
@@ -28,7 +36,8 @@ class Db:
 
         for i in res:
             cur.execute(
-                'insert or ignore into estoque(name,q) values(?,?)', (int(i[0]), 0))
+                'insert or ignore into estoque(name,q) values(?,?)',
+                (int(i[0]), 0))
         cur.execute('''
                     update estoque
                     set q = ?
@@ -49,3 +58,19 @@ class Db:
         pp(total)
         pp(simples)
         pp(ml_tax)
+
+    def view_count_prod(self, con: sqlite3.Connection, cur: sqlite3.Cursor):
+        cur.execute('''
+            select id, count(qnt), name from estoque
+            where entrada = 0 and natOp = 'Venda de mercadorias'
+            group by name
+            order by count(id) desc;
+        ''')
+        res = cur.fetchall()
+
+        pp('{:8s} {:70s} {:16s}'.format(
+            '', 'saida', ''), width=160)
+        for i in res:
+            # print(f'{round(float(i[1])/30, 2)} : {i[2]} :\t {i[0]}')
+            pp('{:8f} {:70s} {:16s}'.format(
+                round(float(i[1]/30), 2), i[2], i[0]), width=160)
